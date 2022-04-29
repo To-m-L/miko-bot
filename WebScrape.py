fb53e896174d400a1aea7b37b5e52bc25bfca453
To-m-L <tom8731@gmail.com>
2022-04-29 12:20:04 -0400

    Finished majority of functionalities for finding users' favorites in MyAnimeList

--- WebScrape.py
+++ WebScrape.py
@@ -15,19 +15,64 @@ def is_liveTwitch():
 
     return
 
-def getUserTopAnime(name):
+def getUserTopFavorites(name):
     characters = []
+    animes = []
+    mangas = []
+    people = []
+    companies = []
     url = "https://www.myanimelist.net/profile/" + name
     page = requests.get(url, cookies={'CONSENT': 'YES+42'})
     soup = BeautifulSoup(page.content, "html.parser")
-     
+    favorites = soup.find_all(class_="link bg-center")
+    
+    entries = []
 
-    return
+    for entry in favorites:
+        entries.append(entry.get_attribute_list('href')[0])
+    
+    for entry in entries:
+        if "anime" in entry.split("/"):
+            animes.append(entry.split("/")[-1])
+        elif "character" in entry.split("/"):
+            characters.append(entry.split("/")[-1])
+        elif "people" in entry.split("/"):
+            people.append(entry.split("/")[-1])
+        elif "manga" in entry.split("/"):
+            mangas.append(entry.split("/")[-1])
+        elif "producer" in entry.split("/"):
+            companies.append(entry.split("/")[-1])
 
+    allFavorites = [characters, animes, mangas, people, companies]
+
+    return allFavorites
+
+def getUserFavoriteCharacter(name):
+    characters = getUserTopFavorites(name)[0]
+    return characters
+
+def getUserFavoriteAnimes(name):
+    animes = getUserTopFavorites(name)[1]
+    return animes
+
+def getUserFavoriteMangas(name):
+    mangas = getUserTopFavorites(name)[2]
+    return mangas
+
+def getUserFavoritePeople(name):
+    people = getUserTopFavorites(name)[3]
+    return people
+
+def getUserFavoriteCompanies(name):
+    companies = getUserTopFavorites(name)[4]
+    return companies
 
 # Debug
 if __name__ == "__main__":
     print()
     #print(is_liveYT("https://www.youtube.com/c/LofiGirl/live"))
     #print(is_liveYT("https://www.youtube.com/channel/UC-hM6YJuNYVAmUWxeIr9FeA/live"))
+    print(getUserTopFavorites("terminal_"))
+    print(getUserFavoriteCharacter("terminal_"))
+
     
--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import discord
 import os
 import WebScrape
-import random
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -39,8 +38,83 @@ async def on_message(message):
     
     if (f"apex legends") in message.content:
             await message.add_reaction(u"\U0001F92E")
+    
+    if "$malfav" in message.content:
+        #msg = message.content.split(" ")
+        #if(len(msg) == 2):
+        if message.content.split(" ")[2] == "character":
+            characters = WebScrape.getUserFavoriteCharacter(message.content.split(" ")[1])
 
-    #if message.content == (f"$rps"):
+            count=0
+            for character in characters:
+                count+=1
+                await message.channel.send(str(count) + ". " + character)
+
+        elif message.content.split(" ")[2] == "anime":
+            animes = WebScrape.getUserFavoriteAnimes(message.content.split(" ")[1])
+
+            count=0
+            for anime in animes:
+                count+=1
+                await message.channel.send(str(count) + ". " + anime)
+
+        elif message.content.split(" ")[2] == "manga":
+            mangas = WebScrape.getUserFavoriteMangas(message.content.split(" ")[1])
+
+            count=0
+            for manga in mangas:
+                count+=1
+                await message.channel.send(str(count) + ". " + manga)
+        
+        elif message.content.split(" ")[2] == "people":
+            people = WebScrape.getUserFavoritePeople(message.content.split(" ")[1])
+
+            count=0
+            for person in people:
+                count+=1
+                await message.channel.send(str(count) + ". " + person)
+        
+        elif message.content.split(" ")[2] == "producer":
+            producers = WebScrape.getUserFavoriteCompanies(message.content.split(" ")[1])
+
+            count=0
+            for producer in producers:
+                count+=1
+                await message.channel.send(str(count) + ". " + producer)
+        """
+        elif(len(msg) == 3):
+            everything = WebScrape.getUserTopFavorites(message.content.split(" ")[1])
+
+            count=1
+            for entry in everything:
+                if count==1:
+                    await message.channel.send("Characters:")
+                    for character in entry:
+                        await message.channel.send(character)
+                    count+=1
+                if count==2:
+                    await message.channel.send("Animes:")
+                    for anime in entry:
+                        await message.channel.send(anime)
+                    count+=1
+                if count==3:
+                    await message.channel.send("Mangas:")
+                    for manga in entry:
+                        await message.channel.send(manga)
+                    count+=1
+                if count==4:
+                    await message.channel.send("People:")
+                    for person in entry:
+                        await message.channel.send(person)
+                    count+=1
+                if count==5:
+                    await message.channel.send("Producers:")
+                    for producer in entry:
+                        await message.channel.send(producer)
+                    count+=1
+        else:
+            await message.channel.send("Correct Syntax: $malfav (username) (category)")
+        """
 
 @client.event
 
